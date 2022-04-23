9390f21bc0bb98e53cee975abab85a7cec5a3d6a
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-04-23 15:57:35 -0700

    passing all tests on manager class in  Manager.js

--- Develop copy/lib/Manager.js
+++ Develop copy/lib/Manager.js
@@ -1,2 +1,21 @@
 // create a manager class that inherits all the properties of the employee class 
 //and has an additional property for office number
+
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber){
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+    }
+
+    getRole(){
+        return "Manager";
+    }
+
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+}
+
+module.exports = Manager;
